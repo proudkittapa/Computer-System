2e1ba3262186cd597f8f76250b4f60435065d4ad
proudkittapa <proudy2013@gmail.com>
2021-03-28 21:50:46 +0700

    edit users number

--- clientM2.go
+++ clientM2.go
@@ -19,7 +19,7 @@ type Messagee struct {
 }
 
 var mutex sync.Mutex
-var users int = 30000
+var users int = 23000
 
 func send6(conn net.Conn, host string, m string, p string, userId int) {
 	// fmt.Println("sent:", userid)
