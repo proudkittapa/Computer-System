9b4bd3ff74fd8219f96370ab2469343db3bbe401
m_pitsinee <mindpitsinee2001@gmail.com>
2021-03-30 16:35:02 +0700

    run but not work yet

--- pitsinee/serial.go
+++ pitsinee/serial.go
@@ -7,10 +7,10 @@ import (
 	"strconv"
 
 	//"sync"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	//"github.com/jackc/pgx/v4"
+	//_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var (
@@ -22,13 +22,11 @@ var (
 func getQuantity(t chan int, id int) {
 	tx, err := db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
 	if err != nil {
-		fmt.Println("e1")
 		panic(err)
 	}
 	tx.Exec("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE")
 	rows := tx.QueryRow("select name, quantity_in_stock, unit_price from products where product_id = " + strconv.Itoa(id))
 	if err != nil {
-		fmt.Println("e3")
 		tx.Rollback()
 		return
 	}
@@ -40,13 +38,13 @@ func getQuantity(t chan int, id int) {
 	fmt.Println("name: ", name, " quantity: ", quantity, " price: ", price)
 	if err := tx.Commit(); err != nil {
 		fmt.Printf("Failed to commit tx: %v\n", err)
+
 	}
 }
 
 func decrement(t chan int, transactionC chan int, orderQuantity int, id int) {
 	tx2, err := db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
 	if err != nil {
-		fmt.Println("e2")
 		panic(err)
 	}
 	tx2.Exec("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE")
@@ -65,7 +63,7 @@ func decrement(t chan int, transactionC chan int, orderQuantity int, id int) {
 		tx2.Rollback()
 		return
 	}
-	fmt.Println("updated")
+	//fmt.Println("updated")
 	transactionC <- 0
 	if err := tx2.Commit(); err != nil {
 		fmt.Printf("Failed to commit tx2: %v\n", err)
@@ -76,7 +74,6 @@ func insert(user string, id int, q int) {
 
 	tx3, err := db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
 	if err != nil {
-		fmt.Println("e4")
 		panic(err)
 	}
 	tx3.Exec("set transaction isolation level SERIALIZABLE")
@@ -90,14 +87,14 @@ func insert(user string, id int, q int) {
 func preorder(end chan int, user string, productId int, orderQuantity int) {
 	// fmt.Printf("start\n")
 	db.Exec("update products set quantity_in_stock = ? where product_id = ? ", 1000, 1)
-	start := time.Now()
+	//start := time.Now()
 	transactionC := make(chan int)
 	t := make(chan int)
 	go getQuantity(t, productId)
 	go decrement(t, transactionC, orderQuantity, productId)
 	<-transactionC // wait for all go routines
 	go insert(user, productId, orderQuantity)
-	fmt.Printf("time: %v\n", time.Since(start))
+	//fmt.Printf("time: %v\n", time.Since(start))
 	num, _ := strconv.Atoi(user)
 	end <- num
 	return
@@ -105,9 +102,10 @@ func preorder(end chan int, user string, productId int, orderQuantity int) {
 }
 func main() {
 	db, _ = sql.Open("mysql", "root:mind10026022@tcp(127.0.0.1:3306)/prodj")
+	ctx = context.Background()
 
 	end := make(chan int)
-	for i := 1; i < 100; i++ {
+	for i := 1; i < 10; i++ {
 		go preorder(end, strconv.Itoa(i), 1, 5)
 	}
 	for i := range end {
