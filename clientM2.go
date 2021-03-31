a72260783ba8bd395e291c3f35d91fd610217dcf
proudkittapa <proudy2013@gmail.com>
2021-03-31 23:45:51 +0700

    edit /payment

--- clientM2.go
+++ clientM2.go
@@ -135,7 +135,7 @@ func createH(methodd string, pathh string, u int) string {
 func createHP(u int) string {
 	userID := u
 	method := "POST"
-	path := "/products/" + str(rand.Intn(100))
+	path := "/products/" + string(rand.Intn(100))
 	host := "209.97.165.170:8080"
 	contentLength := 20
 	contentType := "application/json"
