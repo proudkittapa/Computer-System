98e4ee4c0b936ad3cdca7c39f7f85e2d9e26c20f
proudkittapa <proudy2013@gmail.com>
2021-04-03 20:14:00 +0700

    edit users number

--- client.go
+++ client.go
@@ -33,7 +33,7 @@ type PayInfo struct {
 }
 
 var mutex sync.Mutex
-var users int = 100
+var users int = 1
 var c = 0
 
 //209.97.165.170
@@ -118,8 +118,8 @@ func main() {
 		//client6(&wg, "GET", "/", i) //30000
 		//client6(&wg, "GET", "/text", i)
 		// go client6(&wg, "GET", "/products", i)
-		client6(&wg, "GET", "/products/1", i)
-		//	go client6(&wg, "POST", "/products/1")
+		// client6(&wg, "GET", "/products/1", i)
+		client6(&wg, "POST", "/products/1", i)
 	}
 	wg.Wait()
 	// time.Sleep(100 * time.Millisecond)
