fa3710ef14ffa91ee050ea2c30d82653faf099a5
proudkittapa <proudy2013@gmail.com>
2021-04-03 19:23:38 +0700

    add db

--- cacheFile/lru.go
+++ cacheFile/lru.go
@@ -51,6 +51,9 @@ type JsonCache struct {
 }
 
 func Cache_cons(cap int) Lru_cache {
+	db, _ = sql.Open("mysql", "root:62011139@tcp(localhost:3306)/prodj")
+	// db.SetMaxIdleConns(200000)
+	db.SetMaxOpenConns(200000)
 	return Lru_cache{limit: cap, mp: make(map[int]*Node, cap)}
 }
 
