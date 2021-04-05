13a5214ea671cd65c76ada2fb66da8f74ae56460
proudkittapa <proudy2013@gmail.com>
2021-04-05 09:23:36 +0700

    -

--- client.go
+++ client.go
@@ -118,10 +118,10 @@ func main() {
 	for i := 0; i < users; i++ {
 		wg.Add(1)
 		// go client6(&wg, "POST", "/payment", i)
-		// go client6(&wg, "GET", "/", i) //30000
+		go client6(&wg, "GET", "/", i) //30000
 		//client6(&wg, "GET", "/text", i)
 		// go client6(&wg, "GET", "/products", i)
-		go client6(&wg, "GET", "/products/1", i)
+		// go client6(&wg, "GET", "/products/1", i)
 		// go client6(&wg, "POST", "/products/1", i)
 	}
 	wg.Wait()
