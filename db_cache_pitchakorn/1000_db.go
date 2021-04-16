0943439974cd0e620c3743fe572e5f4a2fa5ac49
pitipitpit <62011212@kmitl.ac.th>
2021-04-16 14:19:15 +0700

    create db

---
+++ db_cache_pitchakorn/1000_db.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// func add_db() {
+
+// }
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:62011212@tcp(127.0.0.1:3306)/prodj")
+	checkErr(err)
+
+	for i := 0; i < 100; i++ {
+		insert, err := db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Shirt', '10000', 20) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Swater', '10000', 30) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Hoodie', '10000', 25) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Shoes', '10000', 10) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Pants', '10000', 20) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Shirt', '10000', 20) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Swater', '10000', 30) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Hoodie', '10000', 25) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Shoes', '10000', 10) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Pants', '10000', 20) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Shirt', '10000', 20) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Swater', '10000', 30) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Hoodie', '10000', 25) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Shoes', '10000', 10) ")
+		insert, err = db.Query("INSERT INTO products (name, quantity_in_stock, unit_price) VALUE ('Pants', '10000', 20) ")
+
+		insert.Close()
+		checkErr(err)
+
+		fmt.Println("done")
+	}
+}
