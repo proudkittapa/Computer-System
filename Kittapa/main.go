7e1ee3c8cd8d60ed526fe42c54813fbf5ce8dee8
proudkittapa <proudy2013@gmail.com>
2021-04-22 23:20:30 +0700

    -

--- Kittapa/main.go
+++ Kittapa/main.go
@@ -10,17 +10,29 @@ type (
 	}
 
 	Route struct {
-		Method string `json:"method"`
-		Path   string `json:"path"`
-		Name   string `json:"name"`
+		Method string      `json:"method"`
+		Path   string      `json:"path"`
+		Name   HandlerFunc `json:"name"`
 	}
 )
 type HandlerFunc func(context.Context) error
 
-func (s *Server) GET(path string, h HandlerFunc) string {
-	return s.Add(path)
+func (s *Server) GET(path string, h HandlerFunc) *Route {
+	m := "GET"
+	return s.Add(m, path, h)
 }
 
-func (s *Server) Add(path string) string {
+func (s *Server) POST(path string, h HandlerFunc) *Route {
+	m := "POST"
+	return s.Add(m, path, h)
+}
 
+func (s *Server) Add(m, path string, h HandlerFunc) *Route {
+	r := Route{
+		Method: m,
+		Path:   path,
+		Name:   h,
+	}
+	s.routers[path] = r
+	return r
 }
