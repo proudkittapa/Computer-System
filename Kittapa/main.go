9cf1e840fb372b1e948220fc4e3e96da9bf8ab76
proudkittapa <proudy2013@gmail.com>
2021-04-22 23:09:26 +0700

    Get and post

--- Kittapa/main.go
+++ Kittapa/main.go
@@ -1,85 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net"
-	"os"
-	"strings"
+	"context"
 )
 
-type Handler func()
-
-func Handle(conn net.Conn) {
-	defer conn.Close()
-	req(conn)
-}
-
-func req(conn net.Conn) {
-	buffer := make([]byte, 1024)
-	for {
-		n, err := conn.Read(buffer)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		Receive(string(buffer[:n]))
+type (
+	Server struct {
+		routers map[string]Route
 	}
 
-}
-
-func main() {
-	li, err := net.Listen("tcp", ":8080")
-	// db.SetMaxIdleConns(200000)
-
-	if err != nil {
-		log.Fatalln(err.Error())
+	Route struct {
+		Method string `json:"method"`
+		Path   string `json:"path"`
+		Name   string `json:"name"`
 	}
-	defer li.Close()
-	for {
-		conn, err := li.Accept()
+)
+type HandlerFunc func(context.Context) error
 
-		if err != nil {
-			log.Fatalln(err.Error())
-			continue
-		}
-		// fmt.Println("connections:", count)
-		go Handle(conn)
-	}
+func (s *Server) GET(path string, h HandlerFunc) string {
+	return s.Add(path)
 }
 
-func Receive(message string) (method string, pathS string, pathL []string) {
-	// if !strings.Contains(message, "HTTP") {
-	// 	if _, err := conn.Write([]byte("Recieved\n")); err != nil {
-	// 		log.Printf("failed to respond to client: %v\n", err)
-	// 	}
-	// 	break
-	// }
-	fmt.Println(message)
-	headers := strings.Split(message, "\n")
-	method = (strings.Split(headers[0], " "))[0]
-	pathS = (strings.Split(headers[0], " "))[1]
-	pathL = strings.Split(pathS, "/")
-	// fmt.Println("headers:", headers)
-	fmt.Println("method:", method)
-	fmt.Println("path:", pathS)
-	fmt.Println("p:", pathL)
-	return method, pathS, pathL
-}
+func (s *Server) Add(path string) string {
 
-func GET(path string, do Handler) {
-	do()
-}
-
-func d(d string) {
-	fmt.Println("helooo")
-}
-
-func send(conn net.Conn, d string, c string) {
-	fmt.Fprintf(conn, createHeader(d, c))
-}
-
-func createHeader(d string, contentType string) string {
-	contentLength := len(d)
-	headers := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Length: %d\r\nContent-Type: %s\r\n\n%s", contentLength, contentType, d)
-	return headers
 }
