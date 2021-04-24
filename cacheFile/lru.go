c8763e39e7d077a9d8728f2a9d1260b366ea113e
proudkittapa <proudy2013@gmail.com>
2021-04-24 15:33:13 +0700

    -

--- cacheFile/lru.go
+++ cacheFile/lru.go
@@ -71,11 +71,11 @@ func (list *Lru_cache) ReCache(id int) (val string) {
 
 }
 
-func Cache_cons(cap int) Lru_cache {
+func Cache_cons(cap int) *Lru_cache {
 	db, _ = sql.Open("mysql", "root:62011139@tcp(localhost:3306)/prodj")
 	// // db.SetMaxIdleConns(200000)
 	// db.SetMaxOpenConns(200000)
-	return Lru_cache{limit: cap, mp: make(map[int]*Node, cap)}
+	return &Lru_cache{limit: cap, mp: make(map[int]*Node, cap)}
 }
 
 func (list *Lru_cache) GetCache(id int) string {
