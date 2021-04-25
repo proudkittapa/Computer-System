325d11fce9294bbda905a468d21d25ec2aee4f56
proudkittapa <proudy2013@gmail.com>
2021-04-25 17:20:44 +0700

    0

--- Nichapatr/client_testing.go
+++ Nichapatr/client_testing.go
@@ -316,8 +316,8 @@ func main() {
 	// flag.Parse()
 	var wg1 sync.WaitGroup
 	start := time.Now()
-	misshit_check(wg1)
-	// test_time_check()
+	// misshit_check(wg1)
+	test_time_check(wg1)
 	// onerun2(&wg1)
 	// start := time.Now()
 	// user_model(&wg1)
