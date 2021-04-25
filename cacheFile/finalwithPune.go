7b317e06e38928231e0474d668d7625537175e3f
m_pitsinee <mindpitsinee2001@gmail.com>
2021-04-25 20:06:44 +0700

    set con

--- cacheFile/finalwithPune.go
+++ cacheFile/finalwithPune.go
@@ -35,8 +35,8 @@ type product struct {
 func InitDatabase() {
 	// db, _ = sql.Open("mysql", "root:mind10026022@tcp(127.0.0.1:3306)/prodj")
 	db, _ = sql.Open("mysql", "root:62011139@tcp(127.0.0.1:3306)/prodj")
-	db.SetMaxOpenConns(100)
-	db.SetMaxIdleConns(200)
+	db.SetMaxOpenConns(32000)
+	db.SetMaxIdleConns(32000)
 	for i := 1; i <= 5; i++ {
 		db.Exec("update products set quantity_in_stock = ? where product_id = ? ", 1000, i)
 	}
