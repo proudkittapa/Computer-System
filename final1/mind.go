dd6eb277127a2d45859a63b816f58db43ba18d65
m_pitsinee <mindpitsinee2001@gmail.com>
2021-04-26 03:53:46 +0700

    please work

--- final1/mind.go
+++ final1/mind.go
@@ -1,4 +1,4 @@
-package main
+package final1
 
 import (
 	"database/sql"
@@ -13,9 +13,10 @@ var (
 	db          *sql.DB
 	q           int
 	newQuantity int
+	result      string
 )
 
-func getQuantity(id int) {
+func GetQuantity(id int) {
 	row, err := db.Query("select name, quantity_in_stock, unit_price from products where product_id = " + strconv.Itoa(id))
 	if err != nil {
 		panic(err)
@@ -29,36 +30,51 @@ func getQuantity(id int) {
 		fmt.Println("name: ", name, " quantity: ", quantity, " price: ", price)
 	}
 }
-func decrement(orderQuantity int, id int) {
+func Decrement(orderQuantity int, id int) {
 	newQuantity := q - orderQuantity
 	if newQuantity < 0 {
+		result = "order more than stock quantity"
 		return
+	} else if q == 0 {
+		result = "The order is out of stock"
+		return
+	} else {
+		result = "transaction successful"
+		fmt.Println("new quantity: ", newQuantity)
+		db.Exec("update products set quantity_in_stock = ? where product_id = ? ", newQuantity, id)
 	}
-	fmt.Println("new quantity: ", newQuantity)
-	db.Exec("update products set quantity_in_stock = ? where product_id = ? ", newQuantity, id)
-
 	return
+
 }
 
-func insert(user string, id int, q int) {
+func Insert(user string, id int, q int) {
 	db.Exec("INSERT INTO order_items(username, product_id, quantity) VALUES (?, ?, ?)", user, id, q)
 }
 
-func preorder(user string, productId int, orderQuantity int) {
+func Preorder(user string, productId int, orderQuantity int) {
 	start := time.Now()
-	insert(user, productId, orderQuantity)
-	getQuantity(productId)
-	decrement(orderQuantity, productId)
+	GetQuantity(productId)
+	Decrement(orderQuantity, productId)
+	Insert(user, productId, orderQuantity)
 	fmt.Printf("time: %v\n", time.Since(start))
 	return
 }
-func main() {
-	db, _ = sql.Open("mysql", "root:mind10026022@tcp(127.0.0.1:3306)/prodj")
-	start := time.Now()
-	for i := 1; i < 10; i++ {
-		go preorder(strconv.Itoa(i), 1, 1)
-		fmt.Printf("Total time: %v\n", time.Since(start))
-	}
-	fmt.Printf("Total time: %v\n", time.Since(start))
 
+func PostPreorder(id int, quantity int) string {
+	Preorder(strconv.Itoa(1), id, quantity)
+	return result
 }
+
+// func main() {
+// 	db, _ = sql.Open("mysql", "root:mind10026022@tcp(127.0.0.1:3306)/prodj")
+// 	db.Exec("update products set quantity_in_stock = ? where product_id = ? ", 1000, 1)
+// 	start := time.Now()
+// 	n := 10
+// 	for i := 0; i < n; i++ {
+// 		Preorder(strconv.Itoa(i), 1, 1)
+// 	}
+
+// 	fmt.Printf("Total time: %v\n", time.Since(start))
+// 	return
+
+// }
--- final2/mind2.go
+++ final2/mind2.go
@@ -1,4 +1,4 @@
-package main
+package final2
 
 import (
 	"database/sql"
@@ -11,11 +11,12 @@ import (
 )
 
 var (
-	db    *sql.DB
-	mutex sync.Mutex
+	db     *sql.DB
+	mutex  sync.Mutex
+	result string
 )
 
-func getQuantity(t chan int, id int) {
+func GetQuantity(t chan int, id int) {
 
 	row, err := db.Query("select name, quantity_in_stock, unit_price from products where product_id = " + strconv.Itoa(id))
 	if err != nil {
@@ -33,54 +34,80 @@ func getQuantity(t chan int, id int) {
 		fmt.Println("name: ", name, " quantity: ", quantity, " price: ", price)
 	}
 }
-func decrement(t chan int, transactionC chan int, orderQuantity int, id int) {
+func Decrement(t chan int, transactionC chan string, orderQuantity int, id int) {
 	quantity := <-t // channel from getQuantity
 	newQuantity := quantity - orderQuantity
+	if quantity == 0 {
+		transactionC <- "out of stock"
+		return
+	}
 	if newQuantity < 0 {
-		transactionC <- 0
+		//fmt.Println("the order is out of stock")
+		transactionC <- "not complete"
 		return
 	}
 	fmt.Println("Product left in stock: ", newQuantity)
 	db.Exec("update products set quantity_in_stock = ? where product_id = ? ", newQuantity, id)
-	transactionC <- 0
+	transactionC <- "done"
 }
 
 func insert(user string, id int, q int) {
 	db.Exec("INSERT INTO order_items(username, product_id, quantity) VALUES (?, ?, ?)", user, id, q)
 }
 
-func preorder(end chan int, user string, productId int, orderQuantity int) {
+func Preorder(end chan string, user string, productId int, orderQuantity int) {
 	// fmt.Printf("start\n")
-	db.Exec("update products set quantity_in_stock = ? where product_id = ? ", 1000, 1)
 	start := time.Now()
-	transactionC := make(chan int)
+	transactionC := make(chan string)
 	t := make(chan int)
 	mutex.Lock()
-	go getQuantity(t, productId)
-	go decrement(t, transactionC, orderQuantity, productId)
-	<-transactionC // wait for all go routines
+	go GetQuantity(t, productId)
+	go Decrement(t, transactionC, orderQuantity, productId)
+	result2 := <-transactionC
 	mutex.Unlock()
-	go insert(user, productId, orderQuantity)
-	fmt.Printf("time: %v\n", time.Since(start))
-
-	num, _ := strconv.Atoi(user)
-	end <- num
-	return
-}
-func main() {
-	db, _ = sql.Open("mysql", "root:mind10026022@tcp(127.0.0.1:3306)/prodj")
-	//defer db.Close()
-	n := 10
-	end := make(chan int, n)
-	start2 := time.Now()
-
-	for i := 0; i < n; i++ {
-		go preorder(end, strconv.Itoa(i), 1, 1)
+	if result2 == "not complete" {
+		result = "order more than stock quantity"
+		fmt.Println(result)
+		end <- result
+		return
+	} else if result2 == "out of stock" {
+		result = "The order is out of stock"
+		fmt.Println(result)
+		end <- result
+		return
+	} else {
+		go insert(user, productId, orderQuantity)
+		fmt.Printf("time: %v\n", time.Since(start))
+		result = "transaction successful"
+		end <- result
+		return
 	}
-	for i := 0; i < n; i++ {
-		<-end
-	}
-	fmt.Printf("Total time: %v\n", time.Since(start2))
-
-	return
 }
+func PostPreorder(id int, quantity int) string {
+
+	end := make(chan string)
+	go Preorder(end, strconv.Itoa(1), id, quantity)
+	fmt.Printf("quantityyyy: %d\n", quantity)
+	result = <-end
+	fmt.Println("hererreerere")
+	return result
+}
+
+// func main() {
+// 	db, _ = sql.Open("mysql", "root:mind10026022@tcp(127.0.0.1:3306)/prodj")
+// 	db.Exec("update products set quantity_in_stock = ? where product_id = ? ", 1000, 1)
+
+// 	//defer db.Close()
+// 	n := 10
+// 	end := make(chan string)
+// 	start2 := time.Now()
+
+// 	for i := 0; i < n; i++ {
+// 		go Preorder(end, strconv.Itoa(i), 1, 1)
+// 	}
+// 	for i := 0; i < n; i++ {
+// 		<-end
+// 	}
+// 	fmt.Printf("Total time: %v\n", time.Since(start2))
+// 	return
+// }
