73c50b724972bb9c683ddff5b4a577f44f78ae8d
proudkittapa <proudy2013@gmail.com>
2021-04-26 04:44:17 +0700

    -

--- client.go
+++ client.go
@@ -358,7 +358,7 @@ func user_model(wg1 sync.WaitGroup) {
 		wg1.Add(2)
 		go func() {
 			client(&wg1, "GET", "/", 0)
-			// client(&wg1, "GET", "/products?limit=10&offset="+strconv.Itoa(rand.Intn(10)), 0)
+			client(&wg1, "GET", "/products?limit=10&offset=0", 0)
 		}()
 	}
 	// fmt.Println("here")
@@ -366,17 +366,17 @@ func user_model(wg1 sync.WaitGroup) {
 		wg1.Add(3)
 		go func() {
 			client(&wg1, "GET", "/", 0)
-			// client(&wg1, "GET", "/products?limit=10&offset="+strconv.Itoa(rand.Intn(10)), 0)
-			client(&wg1, "GET", "/products/"+strconv.Itoa(rand.Intn(967)), 0)
+			client(&wg1, "GET", "/products?limit=10&offset=0", 0)
+			client(&wg1, "GET", "/products/"+strconv.Itoa(rand.Intn(14)), 0)
 		}()
 	}
 	for i := 0.0; i < (num_user * 0.15); i++ {
 		wg1.Add(4)
 		go func() {
 			client(&wg1, "GET", "/", 0)
-			// client(&wg1, "GET", "/products?limit=10&offset="+strconv.Itoa(rand.Intn(10)), 0)
-			client(&wg1, "GET", "/products/"+strconv.Itoa(rand.Intn(967)), 0)
-			client(&wg1, "POST", "/products/"+strconv.Itoa(rand.Intn(967)), 2)
+			client(&wg1, "GET", "/products?limit=10&offset=0", 0)
+			client(&wg1, "GET", "/products/"+strconv.Itoa(rand.Intn(14)), 0)
+			client(&wg1, "POST", "/products/"+strconv.Itoa(rand.Intn(14)), 2)
 		}()
 	}
 	wg1.Wait()
