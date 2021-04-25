5c41bb34d206119e4b83eddfb76ecc9d2da2ab83
Nichapatr <62011170@kmitl.ac.th>
2021-04-25 16:55:53 +0700

    change

--- Nichapatr/client_testing.go
+++ Nichapatr/client_testing.go
@@ -311,9 +311,10 @@ func misshit_check() {
 func main() {
 	// flag.Parse()
 	// var wg1 sync.WaitGroup
-	start := time.Now()
-	// misshit_check()
+	misshit_check()
 	// test_time_check()
+	onerun2(&wg1)
+	start := time.Now()
 	user_model(&wg1)
 	// for i := 0; i < 1000; i++ {
 	// 	// wg1.Add(1)
