9802a8fd0aa250b01f611c6cbcf8bbff726d323b
proudkittapa <proudy2013@gmail.com>
2021-04-30 14:15:03 +0700

    -

--- client.go
+++ client.go
@@ -157,7 +157,7 @@ func onerun2(wg1 sync.WaitGroup) {
 		go func() {
 			// client(&wg1, "GET", "/", 0)
 			// client(&wg1, "GET", "/products?limit=10&offset=0", 0)
-			client(&wg1, "GET", "/products/1", 0)
+			client(&wg1, "GET", "/products/1"+strconv.Itoa(rand.Intn(14)), 0)
 			// client(&wg1, "POST", "/products/1", 2)
 		}()
 	}
