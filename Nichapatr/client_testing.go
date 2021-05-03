73aad8d99f8c9917badfd6745aa26c73a81f4f55
Nichapatr <62011170@kmitl.ac.th>
2021-05-03 23:45:19 +0700

    change

--- Nichapatr/client_testing.go
+++ Nichapatr/client_testing.go
@@ -487,7 +487,7 @@ func baseline() {
 	fmt.Printf("\n------> TIME t7: %v\n", time.Since(t7))
 }
 func baselineN() {
-	n := 10
+	n := 100
 	t1 := time.Now()
 	for i := 0; i < n; i++ {
 		clientNoGo("GET", "/", 0)
