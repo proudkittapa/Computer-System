32e02f946fb101f8b0c963eaef5b758ea7fe57bb
proudkittapa <proudy2013@gmail.com>
2021-05-04 13:20:22 +0700

    -

--- Nichapatr/client_testing.go
+++ Nichapatr/client_testing.go
@@ -382,7 +382,7 @@ func unpredictcheck(success int) {
 	}
 }
 
-var num_user float64 = 10
+var num_user float64 = 100
 
 func user_model(wg1 sync.WaitGroup) { /*-------------------- user_model --------------------*/
 	t1 := time.Now()
